198017ad954d6ff6249ff5863825f426bdc67527
yjha9649 <yoojin.ha@kt.com>
2025-03-25 10:19:24 +0900

    250325 YJHA 아이스크림 에듀 벤치마크 평가 코드 추가

--- vlmeval/dataset/IceCreamEduBench.py
+++ vlmeval/dataset/IceCreamEduBench.py
@@ -1,17 +1,28 @@
-
-
+from .image_base import ImageBaseDataset
+import pandas as pd
+from .utils.IceCreamEduBench import *
+from ..smp import *
 
 class IceCreamEduBench(ImageBaseDataset):
+
+    DATASET_URL = {
+        'IceCreamEdu_TEST': '/Users/yoojin_ha/Desktop/개발/VLM_Evaluation/VLMEvalKit/LMUData/IceCreamEdu_TEST.tsv',
+    }
+
+    DATASET_MD5 = {
+        'IceCreamEdu_TEST': '599c102c1486e7da9f3737dc286ad89c', # 변경 필요
+        # 'IceCreamEdu_TEST': '6b5a9ce3e6057be4b667ec6551febeef',
+    }
 
     def build_prompt(self, line):
         img_path = line['image_path']
         context = line['context']
-        question = lint['question']
+        question = line['question']
         q_type = line['q_type']
 
         prompt = ''
 
-        if context is not None:
+        if not pd.isna(context):
             prompt += f'지문: {context}\n'
         prompt += f'질문: {question}\n'
 
@@ -42,13 +53,43 @@ class IceCreamEduBench(ImageBaseDataset):
             sqa_prompt = '질문에 대한 정답을 한 단어 또는 짧은 문장으로 작성해 주세요.'
             prompt += sqa_prompt
         elif q_type == "long_answer":
+            lqa_prompt = '질문에 대한 정답을 구체적으로 작성해 주세요.'
+            prompt += sqa_prompt
 
         msgs = []
         if isinstance(img_path, list):
             msgs.extend([dict(type='image', value=p) for p in img_path])
         else:
-            msgs = [dict(type='image', value=image_path)]
+            msgs = [dict(type='image', value=img_path)]
         msgs.append(dict(type='text', value=prompt))
+        return msgs
 
-    # def evaluate(self, eval_file, **judge_kwargs):
+    def evaluate(self, eval_file, **judge_kwargs):
     
+        pred_correct = 0
+        judge_dict = dict()
+
+        data = load(eval_file)
+        dataset = self.dataset_name
+        data['q_type'] = [str(x) for x in data['q_type']]
+        data['prediction'] = [str(x) for x in data['prediction']]
+        data['answer'] = [str(x) for x in data['answer']]
+        lt = len(data)
+        pool = mp.Pool(16)
+        lines = [data.iloc[i] for i in range(lt)]
+
+        for line in lines:
+            q_type = line['q_type']
+            answer = line['answer']
+            prediction = line['prediction']
+            
+            correct = eval_multi_choice(answer, prediction)
+
+            if correct:
+                judge_dict[line['data_id']] = 'Correct'
+                pred_correct += 1
+            else:
+                judge_dict[line['data_id']] = 'Wrong'
+        
+        return judge_dict, {'acc': pred_correct / len(lines)}
+
