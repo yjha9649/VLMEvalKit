3ff2bc7cdb0d3a29489ab234ecdbe69d12800828
yjha9649 <yoojin.ha@kt.com>
2025-03-26 17:00:49 +0900

    250326 YJHA IceCreamEduBench 프롬프트 변경 및 평가 코드 수정

--- vlmeval/dataset/IceCreamEduBench.py
+++ vlmeval/dataset/IceCreamEduBench.py
@@ -5,13 +5,14 @@ from ..smp import *
 
 class IceCreamEduBench(ImageBaseDataset):
 
+    TYPE = 'MCQ'
+
     DATASET_URL = {
         'IceCreamEdu_TEST': '/Users/yoojin_ha/Desktop/개발/VLM_Evaluation/VLMEvalKit/LMUData/IceCreamEdu_TEST.tsv',
     }
 
     DATASET_MD5 = {
-        'IceCreamEdu_TEST': '599c102c1486e7da9f3737dc286ad89c', # 변경 필요
-        # 'IceCreamEdu_TEST': '6b5a9ce3e6057be4b667ec6551febeef',
+        'IceCreamEdu_TEST': 'd46af35bc5357660a76d058d958fda30'
     }
 
     def build_prompt(self, line):
@@ -33,19 +34,19 @@ class IceCreamEduBench(ImageBaseDataset):
             D = line['보기4']
             E = line['보기5']
             
-            options_prompt = ''
-            if A is not None:
-                options_prompt += f'1. {A}\n'
-            if B is not None:
-                options_prompt += f'2. {B}\n'
-            if C is not None:
-                options_prompt += f'3. {C}\n'
-            if D is not None:
-                options_prompt += f'4. {D}\n'
-            if E is not None:
-                options_prompt += f'5. {E}\n'
-            mcq_prompt = '위의 선택지 중 올바른 번호를 선택해 주세요.'
-
+            options_prompt = '선택지:\n'
+            if not pd.isna(A):
+                options_prompt += f'1번. {A}\n'
+            if not pd.isna(B):
+                options_prompt += f'2번. {B}\n'
+            if not pd.isna(C):
+                options_prompt += f'3번. {C}\n'
+            if not pd.isna(D):
+                options_prompt += f'4번. {D}\n'
+            if not pd.isna(E):
+                options_prompt += f'5번. {E}\n'
+            # mcq_prompt = '위의 선택지 중 정답이 되는 번호를 작성해 주세요.\n※ 선택지 번호만 작성하고, 여러 개일 경우 쉼표로 구분해 주세요.\n※ 반드시 선택지 번호와 쉼표 외에는 아무것도 쓰지 마세요. (해설, 설명, 단어 등 금지)\n'
+            mcq_prompt = '질문에 답하고, 정답인 선택지 번호만 출력하세요. (예: 1번, 2번, 3번 등. 복수 정답일 경우 모든 정답 번호를 적으세요.) 해설은 출력하지 마세요.'
             prompt += options_prompt
             prompt += mcq_prompt
             
@@ -61,7 +62,7 @@ class IceCreamEduBench(ImageBaseDataset):
             msgs.extend([dict(type='image', value=p) for p in img_path])
         else:
             msgs = [dict(type='image', value=img_path)]
-        msgs.append(dict(type='text', value=prompt))
+        msgs.append(dict(type='text', value=prompt.strip()))
         return msgs
 
     def evaluate(self, eval_file, **judge_kwargs):
@@ -75,21 +76,28 @@ class IceCreamEduBench(ImageBaseDataset):
         data['prediction'] = [str(x) for x in data['prediction']]
         data['answer'] = [str(x) for x in data['answer']]
         lt = len(data)
-        pool = mp.Pool(16)
+        # pool = mp.Pool(16)
         lines = [data.iloc[i] for i in range(lt)]
 
         for line in lines:
             q_type = line['q_type']
             answer = line['answer']
             prediction = line['prediction']
+
+            if q_type == "multi_choices":
+                prediction = extract_answer_number(prediction)
+                answer = circled_number_to_digit(answer)
+            elif q_type == "short_answer":
+                prediction = extract_short_answer(prediction)
+                answer = extract_short_answer(answer)
             
             correct = eval_multi_choice(answer, prediction)
 
             if correct:
-                judge_dict[line['data_id']] = 'Correct'
+                judge_dict[line['index']] = 'Correct'
                 pred_correct += 1
             else:
-                judge_dict[line['data_id']] = 'Wrong'
+                judge_dict[line['index']] = 'Wrong'
         
         return judge_dict, {'acc': pred_correct / len(lines)}
 
