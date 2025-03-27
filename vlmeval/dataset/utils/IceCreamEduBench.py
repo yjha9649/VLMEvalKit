280dc6bc2d7483e36823f93f3a441c25a4ab9d8e
yjha9649 <yoojin.ha@kt.com>
2025-03-27 09:46:51 +0900

    250327 YJHA MCQA 답변 전처리 코드 추가

--- vlmeval/dataset/utils/IceCreamEduBench.py
+++ vlmeval/dataset/utils/IceCreamEduBench.py
@@ -1,12 +1,38 @@
 import re
 
+# def extract_answer_number(text: str) -> str:
+#     # 다양한 패턴을 포함하는 정규식
+#     match = re.search(r"(정답은|옳은 것은|맞는 것은|답은)\s*(\d+)번", text)
+#     if match:
+#         return match.group(2)
+#     else:
+#         return text  # 매칭 안 될 경우 원문 반환
+
+#### MCQA 평가 방법 ###
+## 답변 전처리
+def circled_number_to_digit(text: str) -> str:
+    circled_number_map = {
+        '①': '1', '②': '2', '③': '3', '④': '4', '⑤': '5'
+    }
+    # 각 문자에 대해 변환, 없으면 그대로 둠
+    return ''.join(circled_number_map.get(char, char) for char in text)
+
+## 예측값 전처리
 def extract_answer_number(text: str) -> str:
-    # 다양한 패턴을 포함하는 정규식
-    match = re.search(r"(정답은|옳은 것은|맞는 것은|답은)\s*(\d+)번", text)
-    if match:
-        return match.group(2)
+    # "숫자+번" 패턴 모두 찾기
+    matches = re.findall(r"(\d+)번", text)
+    if matches:
+        return ", ".join(matches)
     else:
         return text  # 매칭 안 될 경우 원문 반환
+    
+def normalize_answer(answer: str) -> set:
+    # 숫자 추출 → 정수 변환 → 집합
+    return set(map(int, re.findall(r'\d+', answer)))
+
+def is_correct_prediction(pred: str, gt: str) -> bool:
+    return normalize_answer(pred) == normalize_answer(gt)
+##########################
 
 def extract_short_answer(text: str) -> str:
     text = text.strip()
@@ -42,13 +68,10 @@ def extract_short_answer(text: str) -> str:
     return text
 
 
-def circled_number_to_digit(text: str) -> str:
-    circled_number_map = {
-        '①': '1', '②': '2', '③': '3', '④': '4', '⑤': '5'
-    }
-    return circled_number_map.get(text, text)
 
-def eval_multi_choice(answer, prediction):
+
+def eval_multi_choice(prediction, answer):
+
     if answer == prediction:
         correct = True
     else:
